7459af2c2b9fbe4e2092a85d0d1ac285b4c0e16c
Maxim <110911222+Maxeroz@users.noreply.github.com>
2023-06-22 14:33:04 +0400

    Coding Challenge #2

--- assignments.js
+++ assignments.js
@@ -225,3 +225,20 @@ if (neighbours.includes("Germany")) {
 neighbours[neighbours.indexOf("Finland")] = "Republic of Finland";
 console.log(neighbours);
 */
+
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return bill / 100 * 15;
+    } else {
+        return bill / 100 * 20;
+    }
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [calcTip([bills[0]]), calcTip([bills[1]]), calcTip(bills[2])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
